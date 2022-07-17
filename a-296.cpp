83c802acd7b9e54aa9d2607132354d3cbe433b78
elsayeddev <a.elsayed991999@gmail.com>
2022-07-17 18:02:46 +0200

    ..

---
+++ a-296.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+
+    int n;
+    cin >> n;
+    map<int, int> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        m[x]++;
+    }
+    bool valid = true;
+
+    for (const auto &it : m)
+    {
+        if (n < (2 * it.second - 1))
+        {
+            valid = false;
+            break;
+        }
+    }
+    if (valid)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
