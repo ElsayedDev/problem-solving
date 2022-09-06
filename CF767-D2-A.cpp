714e57973fd58af4929aa23af12285b11bd19489
elsayeddev <a.elsayed991999@gmail.com>
2022-09-06 11:30:27 +0200

    --

---
+++ CF767-D2-A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+    int arr[n] = {0};
+
+    int m = n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        arr[x] = 1;
+
+        while (arr[m] == 1)
+        {
+            cout << m << " ";
+            m--;
+        }
+        cout << endl;
+    }
+}
