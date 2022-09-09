23e5f37db53d34e475c519697913ff66d863a565
elsayeddev <a.elsayed991999@gmail.com>
2022-09-09 21:20:44 +0200

    --

--- CF404-D2-A.cpp
+++ CF404-D2-A.cpp
@@ -4,30 +4,69 @@ using namespace std;
 
 int main()
 {
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
-    set<char> se;
     int n;
+    string s;
     cin >> n;
-    while (n--)
+    char this_c = ' ';
+
+    bool is_ok = true;
+
+    for (int i = 0; i < n / 2; i++)
     {
-        string s;
         cin >> s;
-
-        for (int i = 0; i < s.length(); i++)
+        if (i == 0)
         {
+            this_c = s[0];
+        }
+        if (s[i] == s[n - 1 - i])
+        {
+        }
+        else
+        {
+            if (s[i + 1] == s[i])
+            {
 
-            se.insert(s[i]);
+                is_ok = false;
+                break;
+            }
+        }
+    }
+    if (is_ok)
+    {
+        cin >> s;
+        if (!(s[n / 2] == this_c))
+        {
+            is_ok = false;
+        }
+        else
+        {
+            for (int i = 0; i < n / 2; i++)
+            {
+                if ((s[i] != s[n - 1 - i]) || s[i] == this_c || s[n - 1 - i] == this_c)
+                {
+                    is_ok = false;
+                }
+            }
+        }
+    }
+    if (is_ok)
+    {
+        for (int i = n / 2 + 1; i < n; i++)
+        {
+            cin >> s;
+            if (s[i] == s[n - 1 - i])
+            {
+            }
+            else
+            {
+                is_ok = false;
+                break;
+            }
         }
     }
 
-    if (se.size() > 2 || se.size() == 1)
-    {
-        cout << "NO";
-    }
-    else
-    {
-        cout << "YES";
-    }
+    cout << (is_ok ? "YES" : "NO");
 }
