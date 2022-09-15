f696e5c95fb300c8a5ed33deab4ff73761abc62f
elsayeddev <a.elsayed991999@gmail.com>
2022-09-15 19:05:40 +0200

    --

---
+++ CF579-D2-A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    long long int x, ans = 0;
+    cin >> x;
+
+    while (x > 0)
+    {
+        if (x % 2 == 1)
+        {
+            ans++;
+        }
+        x /= 2;
+    }
+    cout<<ans;
+}
