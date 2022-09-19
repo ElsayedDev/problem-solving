0d2396857d843b5e8bb0bb19e30091a7f78f60b8
elsayeddev <a.elsayed991999@gmail.com>
2022-09-19 11:41:47 +0200

    --

--- codechef/ERROR.cpp
+++ codechef/ERROR.cpp
@@ -4,20 +4,8 @@
 
 using namespace std;
 
-
-
-
-int main()
+void first_search(ll t, string p1, string p2)
 {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-
-    const string p1 = "010";
-    const string p2 = "101";
-
-    int t;
-    cin >> t;
-
 next:
     while (t--)
     {
@@ -36,4 +24,60 @@ next:
 
         cout << "Bad" << endl;
     }
+}
+
+void second_search(ll t, string p1, string p2)
+{
+
+next:
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s.substr(i, 2) == p1.substr(0,2))
+            {
+                if (s.substr(i + 2, p1.length() - 2) == p1.substr(2))
+                {
+                    cout << "Good" << endl;
+                    goto next;
+                }
+                else
+                {
+                    i = i + 2;
+                }
+            }
+
+            else if (s.substr(i, 2) == p2.substr(0,2))
+            {
+                if (s.substr(i + 2, p2.length() - 2) == p2.substr(2))
+                {
+                    cout << "Good" << endl;
+                    goto next;
+                }
+                else
+                {
+                    i = i + 2;
+                }
+            }
+        }
+
+        cout << "Bad" << endl;
+    }
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    const string p1 = "010";
+    const string p2 = "101";
+
+    int t;
+    cin >> t;
+
+    second_search(t, p1, p2);
 }
