22e7b17c49f694741045b169baa8e49bfae52351
elsayeddev <a.elsayed991999@gmail.com>
2022-09-23 17:01:40 +0200

    --

---
+++ codechef/TRACE.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+
+using namespace std;
+
+int main()
+{
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n; 
+        cin>>n;
+
+        int arr[n][n];
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>arr[i][j];
+            }
+        }
+        
+
+    }
+    
+}
