4e4adaaca7be1a259c20dd74fa02c917e3d438af
elsayeddev <a.elsayed991999@gmail.com>
2022-09-25 16:50:02 +0200

    --

---
+++ codechef/BUY1GET1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define SPEED                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+#define ll long long int
+
+using namespace std;
+
+int main()
+{
+    SPEED
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s; 
+        cin>>s;
+        
+    }
+}
