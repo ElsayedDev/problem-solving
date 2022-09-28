34830ada3d5a3a898f9f99762338e24ef7b5b3d1
elsayeddev <a.elsayed991999@gmail.com>
2022-09-28 13:19:51 +0200

    --

---
+++ codechef/FRIEZA.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define SPEED                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+#define ll long long int
+
+using namespace std;
+
+int main()
+{
+    SPEED
+
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            int count = 1;
+            if (s[i] == 0)
+            {
+                continue;
+            }
+            else
+            {
+                for (int j = i + 1; j < s.length(); j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        count++;
+                        s[j] = 0;
+                    }
+                }
+            }
+            s[i] = count;
+        }
+    }
+}
