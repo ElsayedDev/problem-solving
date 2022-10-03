139d668cc2eb82bf3f221b73267e42624b20b0bb
elsayeddev <a.elsayed991999@gmail.com>
2022-10-03 10:28:39 +0200

    --

--- codeforces/CF102-D2-B.cpp
+++ codeforces/CF102-D2-B.cpp
@@ -15,26 +15,21 @@ int main()
     freopen("../input.txt", "r", stdin);
     freopen("../output.txt", "w", stdout);
 
-    ll n = 19;
-    // cin >> n;
+    string s;
+    cin >> s;
 
-    // convert n from decimal to binary
-    string s = "";
-
-    while (n > 0)
+    ll ans = 0;
+    while (s.length() > 1)
     {
-        s += to_string(n % 2);
-        n /= 2;
+        ll sum = 0;
+        for (ll i = 0; i < s.length(); i++)
+        {
+            sum += s[i] - '0';
+        }
+
+        s = to_string(sum);
+
+        ans++;
     }
-
-    cout << s << endl;
-
-    // // convert from binary string to decimal int
-    // ll ans = 0;
-    // s="01111";
-    // for (int i =0; i < s.length(); i++)
-    // {
-    //     ans += (s[i] - '0') * pow(2, i);
-    // }
-    // cout << ans << endl;
+    cout << ans << endl;
 }
