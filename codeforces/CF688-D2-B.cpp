88b7a123b3dc2f52b970528582490c5a8956318b
elsayeddev <a.elsayed991999@gmail.com>
2022-10-11 12:59:59 +0200

    --

---
+++ codeforces/CF688-D2-B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define SPEED                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+#define ll long long int
+
+using namespace std;
+
+int main()
+{
+    SPEED
+
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    string s;
+    cin >> s;
+
+    string f = s;
+
+    // reverse the string s
+    reverse(s.begin(), s.end());
+
+    cout << f + s << endl;
+}
