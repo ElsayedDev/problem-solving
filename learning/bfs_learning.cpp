781a57b5d6f9f6e30373658df673a5a6bf661e72
elsayeddev <elsayeddev@gmail.com>
2022-11-02 18:27:48 +0200

    --

---
+++ learning/bfs_learning.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    map<string, vector<string>> values = {
+        {"s", {"a", "c"}},
+        {"a", {"b", "f"}},
+        {"b", {}},
+        {"c", {"b", "d"}},
+        {"d", {"f"}},
+    };
+    
+
+}
