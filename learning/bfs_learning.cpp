94c7a9eedac8c5568bc5c3809b94565f2282cf94
elsayeddev <elsayeddev@gmail.com>
2022-11-03 07:28:02 +0200

    --

--- learning/bfs_learning.cpp
+++ learning/bfs_learning.cpp
@@ -2,10 +2,15 @@
 
 using namespace std;
 
+bool is_this_the_value(string s)
+{
+    return s == "f";
+}
+
 int main()
 {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
 
     map<string, vector<string>> values = {
         {"s", {"a", "c"}},
@@ -14,6 +19,30 @@ int main()
         {"c", {"b", "d"}},
         {"d", {"f"}},
     };
-    
 
+    queue<vector<string>> qu;
+    qu.push(values["s"]);
+
+    int path = 0;
+    while (!qu.empty())
+    {
+        vector<string> value = qu.front();
+        qu.pop();
+
+        path++;
+        for (int i = 0; i < value.size(); i++)
+        {
+
+            if (is_this_the_value(value[i]))
+            {
+                cout << "shortest path is: " << path << endl;
+                return 0;
+            }
+            else
+            {
+                qu.push(values[value[i]]);
+            }
+        }
+    }
+    cout << ":: :( bad after: " << path << endl;
 }
