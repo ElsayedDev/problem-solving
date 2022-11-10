652787134324e6db3b616f989f2ff652ae822cc1
elsayeddev <elsayeddev@gmail.com>
2022-11-10 15:59:20 +0200

    --

--- learning/main.py
+++ learning/main.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python
 
 
-
 infinity = float("inf")
 graph = {}
 costs = {}
 parents = {}
 processed = []
 
+# this the main graph
+graph["a"]["fin"] = 1
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+graph["fin"] = {}
+
+# the initial cost for each vertices 
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+# parents for each vertices 
 parents["a"] = "start"
 parents["b"] = "start"
 parents["fin"] = None
 
-graph["start"] = {}
-graph["start"]["a"] = 6
-graph["start"]["b"] = 2
-
-costs["start"] = {}
-costs["start"]["a"] = 6
-costs["start"]["b"] = 2
-
 def find_lowest_cost_node():
     lowest_cost = float("inf")
-    lowest_cost_node= None
+    lowest_cost_node = None
     for node in costs:
         cost = costs[node]
         if cost < lowest_cost and node not in processed:
-            lowest_cost =cost
-            lowest_cost_node = node 
+            lowest_cost = cost
+            lowest_cost_node = node
     return lowest_cost_node
-    
+
 
 node = find_lowest_cost_node()
 while node is not None:
@@ -39,6 +42,6 @@ while node is not None:
         new_cost = cost + neighbors[n]
         if costs[n] > new_cost:
             costs[n] = new_cost
-            parents[n] =node
+            parents[n] = node
     processed.append(node)
     node = find_lowest_cost_node()
