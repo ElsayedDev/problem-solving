21769f1e2a3b9db24b420d6e6e8f4d18ad679888
elsayeddev <elsayeddev@gmail.com>
2022-12-20 16:20:00 +0200

    done

---
+++ codechef/AVG.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define SPEED                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+#define ll long long int
+
+using namespace std;
+
+int main()
+{
+    SPEED
+
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k, v;
+        cin >> n >> k >> v;
+
+        int sum = 0, arr[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+            sum += arr[i];
+        }
+
+        int sum_given=  v * (n + k);
+
+        if(sum_given > sum && (sum_given - sum) %k ==0){
+                cout << (sum_given -sum) / k<<endl;
+        }  else {
+            cout << -1 <<endl;
+        }
+    }
+}
