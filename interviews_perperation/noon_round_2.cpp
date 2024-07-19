df5539b171ff3687a0862988e18b1887a20e6c87
Ahmed Elsayed <58011722+ElsayedDev@users.noreply.github.com>
2024-07-19 22:28:13 +0300

    --

--- interviews_perperation/noon_round_2.cpp
+++ interviews_perperation/noon_round_2.cpp
@@ -553,6 +553,28 @@ public:
     }
 
 
+    int countSubstrings(string s) {
+        int counter = 0;
+
+        if (s.length() == 0)
+            return 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            countSubstrings(counter, s, i, i);     // odd length;
+            countSubstrings(counter, s, i, i + 1); // even length
+        }
+
+        return counter;
+    }
+
+    void countSubstrings(int &counter, string s, int l, int r) {
+        while (l >= 0 && r < s.length() && s[l] == s[r]) {
+            counter++;
+            l--;
+            r++;
+        }
+    }
+
 };
 
 
